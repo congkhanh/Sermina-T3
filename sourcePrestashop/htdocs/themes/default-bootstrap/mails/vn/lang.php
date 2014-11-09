544e4b41c825bcfcad6b6850bb87994d4ad3d8bf
Che Cong Khanh <Congkhanh08@gmail.com>
2014-11-09 09:56:21 +0700

    Revert "Version1"
    This reverts commit 676221ef4b890c13c040ae86be6296099c3a1b2d.

--- sourcePrestashop/htdocs/themes/default-bootstrap/mails/vn/lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-global $_LANGMAIL;
-$_LANGMAIL = array();
-
-$_LANGMAIL['Order confirmation'] = 'Xác nhận đặt hàng';
-
-
-return $_LANGMAIL;
--- sourcePrestashop/htdocs/themes/default-bootstrap/modules/statsbestcategories/translations/vn.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{statsbestcategories}default-bootstrap>statsbestcategories_49ee3087348e8d44e1feda1917443987'] = 'Tên';
